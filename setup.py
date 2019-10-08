c2ead906afdbd7031cf19edafadae816833598f0
adetyaz <tayo.lasisi@yahoo.com>
2019-10-08 23:58:16 +0100

    requirements file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Interackt',
+    version='1.0',
+    packages=['posts', 'posts.migrations', 'groups', 'groups.migrations', 'accounts', 'accounts.migrations',
+              'Interackt'],
+    url='',
+    license='',
+    author='Adetayo',
+    author_email='adetayo.lasisi@gmail.com',
+    description='A simple social media clone'
+)
